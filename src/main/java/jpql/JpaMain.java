5dbdd29e66754d6cd9c193b880ca1f3198ae3e8a
KiHwan Yun <drg2524@naver.com>
2024-04-02 19:11:54 +0900

    Named Query Section

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,58 +1,65 @@
-//package jpql;
-//
-//import jakarta.persistence.*;
-//import org.hibernate.mapping.Collection;
-//
-//import java.util.List;
+package jpql;
 
-//public class JpaMain {
-//
-//
-//    public static void main(String[] args) {
-//
-//
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-//        EntityManager em = emf.createEntityManager();
-//
-//        EntityTransaction tx = em.getTransaction();
-//        tx.begin(); //  트랜잭션 시작
-//        try{
-//
-//            Team teamA = new Team();
-//            teamA.setName("팀A");
-//            em.persist(teamA);
-//
-//            Team teamB = new Team();
-//            teamB.setName("팀B");
-//            em.persist(teamB);
-//
-//
-//            Member member1 = new Member();
-//            member1.setUsername("회원1");
-//            member1.setTeam(teamA);
-//            em.persist(member1);
-//            Member member2 = new Member();
-//            member2.setUsername("회원2");
-//            member1.setTeam(teamA);
-//            em.persist(member2);
-//
-//            Member member3 = new Member();
-//            member3.setUsername("회원2");
-//            member1.setTeam(teamB);
-//            em.persist(member3);
-//
-//
-//            em.flush();
-//            em.clear();
+import jakarta.persistence.*;
+import org.hibernate.mapping.Collection;
 
-//            tx.commit(); //  트랜잭션 커밋
-//        } catch (Exception e){
-//            tx.rollback();
-//            e.printStackTrace();
-//        }finally {
-//            em.close();
-//        }
-//        emf.close();
-//    }
-//}
-//
+import java.util.List;
+
+public class JpaMain {
+
+
+    public static void main(String[] args) {
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); //  트랜잭션 시작
+        try{
+
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member1.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원2");
+            member1.setTeam(teamB);
+            em.persist(member3);
+
+            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                    .setParameter("username", "회원1")
+                    .getResultList();
+
+            for (Member member : resultList) {
+                System.out.println("member = " + member);
+            }
+
+            em.flush();
+            em.clear();
+
+            tx.commit(); //  트랜잭션 커밋
+        } catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
+
